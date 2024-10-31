7f2bb3b69e4bfcc5b7223f1e2ed7a38d72e0e7cf
Paulo Henrique dos Santos <paulo.hsantos2@sp.senac.br>
2024-10-31 17:20:17 -0300

    OutPut-Input

---
+++ basics/output_input.rb
@@ -0,0 +1,11 @@
+print "Hello World"; print "I love drinking coffee"
+
+##################################
+
+irb(main):001:0> new_string = gets.chomp
+This is a sentence.
+=> "This is a sentence."
+
+irb(main):002:0> puts new_string
+This is a sentence.
+=> nil
